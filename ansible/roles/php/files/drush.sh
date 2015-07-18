21705b07bd7fef006e05f26840477ba004fb85b2
Dominik Zogg <dominik.zogg@gmail.com>
2015-07-18 15:41:55 +0200

    force execution bit on drush command

--- ansible/roles/php/files/drush.sh
+++ ansible/roles/php/files/drush.sh
@@ -3,6 +3,7 @@
 if [ ! -d "/usr/local/lib/drush" ]; then
     git clone https://github.com/drush-ops/drush.git /usr/local/lib/drush
     ln -s /usr/local/lib/drush/drush /usr/local/bin/drush
+    chmod +x /usr/local/lib/drush/drush
 fi
 
 cd /usr/local/lib/drush
